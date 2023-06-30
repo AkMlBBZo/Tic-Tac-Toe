b70fd6b89fddd96c46a826b874ce7e394bc13e9c
Ilia <101037071+AkMlBBZo@users.noreply.github.com>
2023-06-30 13:58:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,14 @@ BotDB = BotDB("tic-tac-toe.db")
 
 TOKEN = YOUR_BOT_API
 
-
 # символы, которые используются
 SYMBOLS = ['⭕','◻','❌']
 
-
+# ответы бота
+YOUR_TURN = 'Ваш ход'
+YOU_WIN = 'Вы победили'
+YOU_FAIL = 'Вы проиграли'
+DRAW = 'Ничья'
 HELP = 'Бот для игры в крестики-нолики\nСоздатель: @dieyouannoying\n\n' \
             'Используйте команду /start для начала новой игры'
 
